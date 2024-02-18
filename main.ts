841520610224362f0ceb05a13999368637b1e47b
gboyd068 <gboyd068@gmail.com>
2024-02-18 16:20:22 +0000

    migrate to swiftlatex rendering

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 import { App, FileSystemAdapter, MarkdownPostProcessorContext, Plugin, PluginSettingTab, SectionCache, Setting, TFile, TFolder } from 'obsidian';
+import { PdfTeXEngine } from 'PdfTeXEngine';
 import { Md5 } from 'ts-md5';
 import * as fs from 'fs';
 import * as temp from 'temp';
 import * as path from 'path';
-import { exec } from 'child_process';
 
 interface MyPluginSettings {
-	command: string,
+	package_url: string,
 	timeout: number,
 	enableCache: boolean,
 	cache: Array<[string, Set<string>]>;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	command: ``,
+	package_url: `https://texlive2.swiftlatex.com/`,
 	timeout: 10000,
 	enableCache: true,
 	cache: [],
@@ -22,6 +22,7 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 	cacheFolderPath: string;
+	pdfEngine: any;
 
 	cache: Map<string, Set<string>>; // Key: md5 hash of latex source. Value: Set of file path names.
 
@@ -29,6 +30,9 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 		if (this.settings.enableCache) await this.loadCache();
 		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.pdfEngine = new PdfTeXEngine();
+		await this.pdfEngine.loadEngine();
+		this.pdfEngine.setTexliveEndpoint(this.settings.package_url);
 		this.registerMarkdownCodeBlockProcessor("latex", (source, el, ctx) => this.renderLatexToElement(source, el, ctx));
 	}
 
@@ -46,7 +50,7 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadCache() {
-		this.cacheFolderPath = path.join((this.app.vault.adapter as FileSystemAdapter).getBasePath(), this.app.vault.configDir, "obsidian-latex-render-svg-cache\\");
+		this.cacheFolderPath = path.join((this.app.vault.adapter as FileSystemAdapter).getBasePath(), this.app.vault.configDir, "obsidian-swiftlatex-render-pdf-cache\\");
 		if (!fs.existsSync(this.cacheFolderPath)) {
 			fs.mkdirSync(this.cacheFolderPath);
 			this.cache = new Map();
@@ -75,52 +79,78 @@ export default class MyPlugin extends Plugin {
 	async renderLatexToElement(source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) {
 		return new Promise<void>((resolve, reject) => {
 			let md5Hash = this.hashLatexSource(source);
-			let svgPath = path.join(this.cacheFolderPath, `${md5Hash}.svg`);
+			let pdfPath = path.join(this.cacheFolderPath, `${md5Hash}.pdf`);
 
-			// SVG file has already been cached
-			// Could have a case where svgCache has the key but the cached file has been deleted
-			if (this.settings.enableCache && this.cache.has(md5Hash) && fs.existsSync(svgPath)) {
-				console.log("Using cached SVG: ", md5Hash);
-				el.innerHTML = fs.readFileSync(svgPath).toString();
+			// PDF file has already been cached
+			// Could have a case where pdfCache has the key but the cached file has been deleted
+			if (this.settings.enableCache && this.cache.has(md5Hash) && fs.existsSync(pdfPath)) {
+				console.log("Using cached PDF: ", md5Hash);
+				el.innerHTML = `<object data="${pdfPath}" type="application/pdf" width="100%" height="100%"></object>`;
 				this.addFileToCache(md5Hash, ctx.sourcePath);
 				resolve();
 			}
 			else {
-				console.log("Rendering SVG: ", md5Hash);
+				console.log("Rendering PDF: ", md5Hash);
 
-				this.renderLatexToSVG(source, md5Hash, svgPath).then((v: string) => {
+				this.renderLatexToPDF(source, md5Hash, pdfPath).then((pdf: any) => {
 					if (this.settings.enableCache) this.addFileToCache(md5Hash, ctx.sourcePath);
-					el.innerHTML = v;
-					resolve();
+				const pdfblob = new Blob([pdf], { type: 'application/pdf' });
+				const objectURL = URL.createObjectURL(pdfblob);
+				el.innerHTML = `<object data="${objectURL}" type="application/pdf" width="100%" height="100%"></object>`;
+				resolve();
 				}
 				).catch(err => { el.innerHTML = err; reject(err); });
 			}
 		}).then(() => { if (this.settings.enableCache) setTimeout(() => this.cleanUpCache(), 1000); });
 	}
 
-	renderLatexToSVG(source: string, md5Hash: string, svgPath: string) {
+	renderLatexToPDF(source: string, md5Hash: string, pdfPath: string) {
 		return new Promise(async (resolve, reject) => {
 			source = this.formatLatexSource(source);
 
-			temp.mkdir("obsidian-latex-renderer", (err, dirPath) => {
+			temp.mkdir("obsidian-swiftlatex-renderer", (err, dirPath) => {
 				if (err) reject(err);
 				fs.writeFileSync(path.join(dirPath, md5Hash + ".tex"), source);
-				exec(
-					this.settings.command.replace(/{file-path}/g, md5Hash)
-					,
-					{ timeout: this.settings.timeout, cwd: dirPath },
-					async (err, stdout, stderr) => {
-						if (err) reject([err, stdout, stderr]);
-						else {
-							if (this.settings.enableCache) fs.copyFileSync(path.join(dirPath, md5Hash + ".svg"), svgPath);
-							let svgData = fs.readFileSync(path.join(dirPath, md5Hash + ".svg"));
-							resolve(svgData);
-						};
-					},
-				);
+			this.pdfEngine.writeMemFSFile("main.tex", source);
+			this.pdfEngine.setEngineMainFile("main.tex");
+			let r = this.pdfEngine.compileLaTeX();
+			if (r.status != 0) {
+				// manage latex errors
+				reject(r.log);
+			}
+			let pdf = r.pdf;
+			// write to cache
+			fs.writeFileSync(pdfPath, pdf);
+
+			resolve(pdf);
 			})
 		});
 	}
+
+
+	// renderLatexToSVG(source: string, md5Hash: string, svgPath: string) {
+	// 	return new Promise(async (resolve, reject) => {
+	// 		source = this.formatLatexSource(source);
+
+	// 		temp.mkdir("obsidian-latex-renderer", (err, dirPath) => {
+	// 			if (err) reject(err);
+	// 			fs.writeFileSync(path.join(dirPath, md5Hash + ".tex"), source);
+	// 			exec(
+	// 				this.settings.command.replace(/{file-path}/g, md5Hash)
+	// 				,
+	// 				{ timeout: this.settings.timeout, cwd: dirPath },
+	// 				async (err, stdout, stderr) => {
+	// 					if (err) reject([err, stdout, stderr]);
+	// 					else {
+	// 						if (this.settings.enableCache) fs.copyFileSync(path.join(dirPath, md5Hash + ".svg"), svgPath);
+	// 						let svgData = fs.readFileSync(path.join(dirPath, md5Hash + ".svg"));
+	// 						resolve(svgData);
+	// 					};
+	// 				},
+	// 			);
+	// 		})
+	// 	});
+	// }
 
 	async saveCache() {
 		let temp = new Map();
@@ -165,22 +195,22 @@ export default class MyPlugin extends Plugin {
 			if (!hashes_in_file.contains(hash)) {
 				this.cache.get(hash)?.delete(file.path);
 				if (this.cache.get(hash)?.size == 0) {
-					this.removeSVGFromCache(hash);
+					this.removePDFFromCache(hash);
 				}
 			}
 		}
 	}
 
-	removeSVGFromCache(key: string) {
+	removePDFFromCache(key: string) {
 		this.cache.delete(key);
-		fs.rmSync(path.join(this.cacheFolderPath, `${key}.svg`));
+		fs.rmSync(path.join(this.cacheFolderPath, `${key}.pdf`));
 	}
 
 	removeFileFromCache(file_path: string) {
 		for (const hash of this.cache.keys()) {
 			this.cache.get(hash)?.delete(file_path);
 			if (this.cache.get(hash)?.size == 0) {
-				this.removeSVGFromCache(hash);
+				this.removePDFFromCache(hash);
 			}
 		}
 	}
@@ -225,20 +255,20 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
+		containerEl.createEl('h2', { text: 'SwiftLaTeX Render Settings' });
 
 		new Setting(containerEl)
-			.setName('Command to generate SVG')
+			.setName('Package Fetching URL')
 			.addText(text => text
-				.setValue(this.plugin.settings.command.toString())
+				.setValue(this.plugin.settings.package_url.toString())
 				.onChange(async (value) => {
-					this.plugin.settings.command = value;
+					this.plugin.settings.package_url = value;
 					await this.plugin.saveSettings();
 				}));
 
 		new Setting(containerEl)
-			.setName('Enable caching of SVGs')
-			.setDesc("SVGs rendered by this pluing will be kept in `.obsidian/obsidian-latex-render-svg-cache`. The plugin will automatically keep track of used svgs and remove any that aren't being used")
+			.setName('Enable caching of PDFs')
+			.setDesc("PDFs rendered by this pluing will be kept in `.obsidian/obsidian-swiftlatex-render-pdf-cache`. The plugin will automatically keep track of used pdfs and remove any that aren't being used")
 			.addToggle(toggle => toggle
 				.setValue(this.plugin.settings.enableCache)
 				.onChange(async (value) => {
